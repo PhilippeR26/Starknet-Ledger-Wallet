d6605b227b3b9c5ec4d833a4d95099cb5a47546a
PhilippeR26 <philippe.rostan@yahoo.fr>
2025-12-10 10:40:24 +0100

    ci: bump to snjs 9.1.0

--- src/app/(site)/components/client/TopBanner.tsx
+++ src/app/(site)/components/client/TopBanner.tsx
@@ -1,9 +1,6 @@
 "use client";
 import { SquareArrowOutUpRight } from 'lucide-react';
 import { Box, Link } from "@chakra-ui/react"
-// import { constants, types, type types as ert  } from "starknet";
-
-// const tt : ert.RPC.RPCSPEC07.ETransactionVersion2 = types.RPC.RPCSPEC07.ETransactionVersion2.F0;
 
 export default function TopBanner() {
   return (
@@ -24,7 +21,7 @@ export default function TopBanner() {
       color="black"
     >
       Powered by
-      <Link color="blue.700" href='https://starknetjs.com'> Starknet.js v8.5.1<SquareArrowOutUpRight margin-left="2px" /></Link>
+      <Link color="blue.700" href='https://starknetjs.com'> Starknet.js v9.1.0<SquareArrowOutUpRight margin-left="2px" /></Link>
       .
       <Link color="blue.700" href='https://github.com/PhilippeR26/Starknet-Ledger-Wallet'> Source code<SquareArrowOutUpRight margin-left="2px" /></Link>
       .
